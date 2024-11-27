97d365c8c9b303f0d4b53866d4400bb0ed9aab6f
James <kamaujames3705@gmail.com>
2024-11-27 10:21:19 +0300

    checked get response data

--- mysite/settings.py
+++ mysite/settings.py
@@ -37,7 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'record'
+    'record',
+    'rest_framework',
 ]
 
 MIDDLEWARE = [
--- record/models.py
+++ record/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+
+class Customer(models.Model):
+    name = models.CharField(max_length=250)
+    code = models.CharField(max_length=10, unique=True)
+    created_at = models.DateTimeField(auto_now_add=True)
