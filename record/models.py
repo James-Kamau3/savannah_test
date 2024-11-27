ce369b884a4621934b286c37ab7c04790935e29a
James <kamaujames3705@gmail.com>
2024-11-27 11:40:38 +0300

    included customer model

--- record/models.py
+++ record/models.py
@@ -1,8 +1,15 @@
 from django.db import models
-
+import random
 # Create your models here.
+
+def generate_unique_code():
+    while True:
+        code = random.randint(1000, 9999)
+        if not Customer.objects.filter(code=code).exists():
+            return code
 
 class Customer(models.Model):
     name = models.CharField(max_length=250)
-    code = models.CharField(max_length=10, unique=True)
+    code = models.PositiveIntegerField(default=generate_unique_code, unique=True)
     created_at = models.DateTimeField(auto_now_add=True)
+
