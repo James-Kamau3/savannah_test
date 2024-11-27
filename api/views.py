69ecbed4f935671524c3d5781595d5c0143b6302
James <kamaujames3705@gmail.com>
2024-11-27 11:46:23 +0300

    serialized fetched data

--- api/views.py
+++ api/views.py
@@ -1,8 +1,12 @@
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
+from .serializers import CustomerSerializer
+from record.models import Customer
 
 
 @api_view(['GET'])
 def getData(request):
-    customer = {'name': 'Chebukati', 'age': 45}
-    return Response(customer)
+    customer = Customer.objects.all()
+    serializer = CustomerSerializer(customer, many=True)
+    return Response(serializer.data)
+
