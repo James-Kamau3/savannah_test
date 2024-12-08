2fe2276f9662afb09d19f82ce009e51f05347910
James <kamaujames3705@gmail.com>
2024-12-08 20:23:57 +0300

    customer model test

--- record/tests.py
+++ record/tests.py
@@ -1,3 +1,18 @@
 from django.test import TestCase
 
 # Create your tests here.
+from django.test import TestCase
+from record.models import Customer
+
+
+#Test fot customer model
+class CustomerModelTest(TestCase):
+
+    def test_customer_creation(self):
+        customer = Customer.objects.create(
+            name="Kasongo",
+            phone_number="+254757988864"
+        )
+        self.assertEqual(customer.name, "Kasongo")
+        self.assertEqual(customer.phone_number, "+254757988864")
+        self.assertIsInstance(customer, Customer)
