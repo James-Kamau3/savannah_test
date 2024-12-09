b022f4d04f8f8afbd004a636b17181146f9d1847
James <kamaujames3705@gmail.com>
2024-12-09 10:54:16 +0300

    created test for order model

--- record/tests.py
+++ record/tests.py
@@ -2,7 +2,7 @@ from django.test import TestCase
 
 # Create your tests here.
 from django.test import TestCase
-from record.models import Customer
+from record.models import Customer, Order
 
 
 #Test fot customer model
@@ -16,3 +16,20 @@ class CustomerModelTest(TestCase):
         self.assertEqual(customer.name, "Kasongo")
         self.assertEqual(customer.phone_number, "+254757988864")
         self.assertIsInstance(customer, Customer)
+
+# Test for order model
+class OrderModelTest(TestCase):
+    def test_order_creation(self):
+        customer = Customer.objects.create(
+            name="Kasongo",
+            phone_number="+254757988864"
+        )
+        order = Order.objects.create(
+            customer=customer,
+            item="Milo",
+            amount=22.76
+        )
+        self.assertEqual(order.customer.name, "Kasongo")
+        self.assertEqual(order.item, "Milo")
+        self.assertEqual(order.amount, 22.76)
+        self.assertIsInstance(order, Order)
